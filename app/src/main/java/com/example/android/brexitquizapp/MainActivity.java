86ba1735af138ae6e87357b8342d5c3848813096
ansecu <antonioseguracuenca@gmail.com>
2017-04-04 21:29:44 +0200

    edittext question works

--- app/src/main/java/com/example/android/brexitquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/brexitquizapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    //variables globales
+    //global variables
     int totalCorrect = 0;
     int totalWrong = 0;
     int totalNotSelected = 0;
@@ -64,6 +65,66 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void evaluateQuestion2() {
+        //casting views
+        CheckBox q2a1checkbox = (CheckBox) findViewById(R.id.q2_a1_checkbox);
+        CheckBox q2a2checkbox = (CheckBox) findViewById(R.id.q2_a2_checkbox);
+        CheckBox q2a3checkbox = (CheckBox) findViewById(R.id.q2_a3_checkbox);
+        //view results
+        if (q2a2checkbox.isChecked() && q2a3checkbox.isChecked()) {
+            totalCorrect +=1;
+        } else if (!(q2a1checkbox.isChecked() || q2a2checkbox.isChecked() ||
+                q2a3checkbox.isChecked())) {
+            totalNotSelected +=1;
+        } else {
+            totalWrong +=1;
+        }
+    }
+
+    public void evaluateQuestion3() {
+        //casting views
+        RadioButton q3a1RadioButton = (RadioButton) findViewById(R.id.q3_a1_RadioButton);
+        RadioButton q3a2RadioButton = (RadioButton) findViewById(R.id.q3_a2_RadioButton);
+        RadioButton q3a3RadioButton = (RadioButton) findViewById(R.id.q3_a3_RadioButton);
+        //check answer
+        if (q3a1RadioButton.isChecked() || q3a3RadioButton.isChecked()) {
+            totalWrong +=1;
+        } else if (q3a2RadioButton.isChecked()) {
+            totalCorrect +=1;
+        } else {
+            totalNotSelected +=1;
+        }
+    }
+
+    public void evaluateQuestion4() {
+        //casting views
+        RadioButton q4a1RadioButton = (RadioButton) findViewById(R.id.q4_a1_RadioButton);
+        RadioButton q4a2RadioButton = (RadioButton) findViewById(R.id.q4_a2_RadioButton);
+        RadioButton q4a3RadioButton = (RadioButton) findViewById(R.id.q4_a3_RadioButton);
+        //check answer
+        if (q4a1RadioButton.isChecked() || q4a2RadioButton.isChecked()) {
+            totalWrong +=1;
+        } else if (q4a3RadioButton.isChecked()) {
+            totalCorrect +=1;
+        } else {
+            totalNotSelected +=1;
+        }
+    }
+
+    public void evaluateQuestion5() {
+        //casting views
+        RadioButton q5a1RadioButton = (RadioButton) findViewById(R.id.q5_a1_RadioButton);
+        RadioButton q5a2RadioButton = (RadioButton) findViewById(R.id.q5_a2_RadioButton);
+        RadioButton q5a3RadioButton = (RadioButton) findViewById(R.id.q5_a3_RadioButton);
+        //check answer
+        if (q5a1RadioButton.isChecked() || q5a3RadioButton.isChecked()) {
+            totalWrong +=1;
+        } else if (q5a2RadioButton.isChecked()) {
+            totalCorrect +=1;
+        } else {
+            totalNotSelected +=1;
+        }
+    }
 
     private void showToast(CharSequence text) {
         Context context = getApplicationContext();
@@ -73,18 +134,26 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
+    /**
+     * this method is called when pushing "comprobar" button
+     *
+     */
     public void comprobar(View view) {
         //recojer resultados
             evaluateQuestion1();
+            evaluateQuestion2();
+            evaluateQuestion3();
+            evaluateQuestion4();
+            evaluateQuestion5();
         //comprobar resultados
         if (totalNotSelected == 0) {
             //componer toast message
-            CharSequence text = "Tienes: "+ totalCorrect + " aciertos y "
-                    + totalWrong + " errores.";
+            CharSequence text = "Tienes: "+ totalCorrect + " acierto(s) y "
+                    + totalWrong + " error(es).";
             //mostrar toast message
             showToast(text);
         } else {
-            CharSequence text = "Falta(n): " + totalNotSelected + " preguntas sin contestar.";
+            CharSequence text = "Falta(n): " + totalNotSelected + " pregunta(s) por contestar.";
             showToast(text);
         }
 
